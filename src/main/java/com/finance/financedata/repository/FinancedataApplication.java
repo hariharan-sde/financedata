1b1f098b4ba5d5cb9fa29eb358c604d520bbb1c5
hariharan-sde <harisekar1412@gmail.com>
2026-04-06 00:27:37 +0530

    Add files via upload

---
+++ src/main/java/com/finance/financedata/repository/FinancedataApplication.java
@@ -0,0 +1,13 @@
+package com.finance.financedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancedataApplication.class, args);
+	}
+
+}
